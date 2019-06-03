e46f6e61c5bf202d46f6f7d0af4f90c251662ebe
ChipFC <chipfc@gmail.com>
2019-06-03 09:31:03 +0700

    DHT11 update

--- NPNBitKit.ts
+++ NPNBitKit.ts
@@ -1,9 +1,17 @@
 // Add your code here
+// https://github.com/chipfc/NPNBitKit
 
 
 //% color="#AA278D"
 //% groups="['Input', 'Output', 'AnalogInput', 'AnalogOutput', 'ComplexInput', 'ComplexOutput']"
 namespace NPNBitKit {
+    //DHT11 variable
+    let _temperature: number = 0.0
+    let _humidity: number = 0.0
+    let _readSuccessful: boolean = false
+
+
+
     //% block="Nút $pinName| được nhấn?"
     //% group=Input
     //% pinName.fieldEditor="gridpicker"
@@ -145,18 +153,69 @@ namespace NPNBitKit {
     //% active.defl=no
     export function DHT11Temp(pinName: DigitalPin, active: boolean = false): number {
         if (active) {
-            return 0
+            //chuyen qua do F
+            if (_readSuccessful) return _temperature * 1.8 + 32
+            else return -999
         }
         else {
-            return 1
+            if (_readSuccessful) return _temperature
+            else return -999
         }
     }
-    //% block="DHT11: độ ẩm không khí $pinName %"
+    //% block="DHT11: độ ẩm $pinName %"
     //% group=ComplexInput
     //% pinName.fieldEditor="gridpicker"
     //% pinName.fieldOptions.width=220
     //% pinName.fieldOptions.columns=4
     export function DHT11Hum(pinName: DigitalPin): number {
-        return 1
+        if (_readSuccessful) return _humidity
+        else return -999
+    }
+
+    function DHT11query(dataPin: DigitalPin) {
+        //initialize
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+        _humidity = -999.0
+        _temperature = -999.0
+        _readSuccessful = false
+        startTime = input.runningTimeMicros()
+        //request data
+        pins.digitalWritePin(dataPin, 0) //begin protocol
+        basic.pause(18)
+        //if (pullUp) pins.setPull(dataPin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(dataPin)
+        while (pins.digitalReadPin(dataPin) == 1);
+        while (pins.digitalReadPin(dataPin) == 0); //sensor response
+        while (pins.digitalReadPin(dataPin) == 1); //sensor response
+        //read data (5 bytes)
+        for (let index = 0; index < 40; index++) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            control.waitMicros(28)
+            //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+            if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+        }
+        endTime = input.runningTimeMicros()
+        //convert byte number array to integer
+        for (let index = 0; index < 5; index++)
+            for (let index2 = 0; index2 < 8; index2++)
+                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+        //verify checksum
+        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+        checksum = resultArray[4]
+        if (checksumTmp >= 512) checksumTmp -= 512
+        if (checksumTmp >= 256) checksumTmp -= 256
+        if (checksum == checksumTmp) _readSuccessful = true
+
+        _humidity = resultArray[0] + resultArray[1] / 100
+        _temperature = resultArray[2] + resultArray[3] / 100
+
     }
 }
